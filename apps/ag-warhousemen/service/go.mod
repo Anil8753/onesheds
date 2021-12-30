7a64e8edf04050031aafbd837adb12e75517eea1
Anil <anilkumar8753@gmail.com>
2021-12-30 19:46:41 +0530

    debug setup and env vars

--- apps/ag-warhousemen/service/go.mod
+++ apps/ag-warhousemen/service/go.mod
@@ -28,6 +28,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/hashicorp/go-uuid v1.0.2
 	github.com/hyperledger/fabric-gateway v1.0.0
+	github.com/joho/godotenv v1.4.0
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
--- apps/ag-warhousemen/service/httpd/setup.go
+++ apps/ag-warhousemen/service/httpd/setup.go
@@ -1,28 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func Setup() {
 
-	os.Setenv("TOKEN_HOUR_LIFESPAN", "1")
-	os.Setenv("API_SECRET", "anilkumar")
+	// os.Setenv("TOKEN_HOUR_LIFESPAN", "1")
+	// os.Setenv("API_SECRET", "anilkumar")
 
-	os.Setenv("TLS_CERT_PATH", "/Users/anikumar/Development/MyProjets/github/onesheds/dev/vars/keyfiles/peerOrganizations/warehousemen.onesheds.com/peers/peer0.warehousemen.onesheds.com/tls/ca.crt")
-	os.Setenv("PEER_ENDPOINT", "localhost:7003")
-	os.Setenv("PEER_URL", "peer0.warehousemen.onesheds.com")
-
+	// os.Setenv("TLS_CERT_PATH", "/Users/anikumar/Development/MyProjets/github/onesheds/dev/vars/keyfiles/peerOrganizations/warehousemen.onesheds.com/peers/peer0.warehousemen.onesheds.com/tls/ca.crt")
+	// os.Setenv("PEER_ENDPOINT", "localhost:7003")
+	// os.Setenv("PEER_URL", "peer0.warehousemen.onesheds.com")
+	err := godotenv.Load("service.env")
+	if err != nil {
+		log.Fatal("Error loading service.env file")
+	}
+	fmt.Println(os.Getenv("TLS_CERT_PATH"), os.Getenv("PEER_URL"))
+	//
 	gin.SetMode(gin.DebugMode)
 
 	// Disable Console Color, you don't need console color when writing the logs to file.
 	gin.DisableConsoleColor()
 
 	// Logging to a file.
-	f, _ := os.Create("gin.log")
+	f, _ := os.Create("service.log")
 	//gin.DefaultWriter = io.MultiWriter(f)
 
 	// Use the following code if you need to write the logs to file and console at the same time.
