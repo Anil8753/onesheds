4791d72cf598ffde2f64a021268dc29a663154fa
Anil <anilkumar8753@gmail.com>
2021-12-31 22:35:21 +0530

    Update error-msg.service.ts

--- apps/ag-warhousemen/web/src/app/services/error-msg.service.ts
+++ apps/ag-warhousemen/web/src/app/services/error-msg.service.ts
@@ -8,8 +8,8 @@ export class ErrorMsgService {
   errors = new Map<number, string>([
     [0, 'Action is sucessfull' ],
     [1, 'Login failed. Please check your user id and password'],
-    [2, 'User id already exist. Please use "Forget Password" option to reset your password'],
-    [3, 'User id does not eixst. Please check user id and try again'],
+    [2, 'User already exist. Please use "Forget Password" option to reset your password'],
+    [3, 'User does not eixst. Please check user id and try again'],
     [4, 'User is not authorized'],
     [5, 'Invalid request, please try after some time'],
     [6, 'Registration failed because server has issue, please try after some time'],
