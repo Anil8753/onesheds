b9b1909d2b2a37065b092471808cb5a9deeeefbb
Anil <anilkumar8753@gmail.com>
2022-01-16 16:51:01 +0530

    refactoring

--- apps/ag-warhousemen/service/httpd/main.go
+++ apps/ag-warhousemen/service/httpd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,4 +17,16 @@ func main() {
 	InitRoutes(engine, hDependencies)
 
 	engine.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func PrintConfig() {
+
+	log.Println("---------------------------------- Configs ----------------------------------")
+	log.Println("TLS_CERT_PATH:", os.Getenv("TLS_CERT_PATH"))
+	log.Println("PEER_ENDPOINT:", os.Getenv("PEER_ENDPOINT"))
+	log.Println("PEER_URL:", os.Getenv("PEER_URL"))
+	log.Println("LEDGER_CHANNEL:", os.Getenv("LEDGER_CHANNEL"))
+	log.Println("LEDGER_CHAINCODE:", os.Getenv("LEDGER_CHAINCODE"))
+	log.Println("NODE_TYPE:", os.Getenv("NODE_TYPE"))
+	log.Println("-----------------------------------------------------------------------------")
 }
